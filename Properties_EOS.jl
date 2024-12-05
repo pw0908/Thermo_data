099def4c5a01d58f7ea6dc2a42553cbbf25e900a
pw0908 <pjwalker@caltech.edu>
2024-12-04 17:14:26 -0800

    Clean-up code so that it handles all calculations at once

--- Properties_EOS.jl
+++ Properties_EOS.jl
@@ -4,63 +4,13 @@ CoolProp_ = pyimport("CoolProp")
 include("iPCSAFT.jl")
 include("TVTPR.jl")
 
-handle = CoolProp_.AbstractState("HEOS", "R40")
+handle = CoolProp_.AbstractState("HEOS", "R134a")
 Tmin = CoolProp_.AbstractState.Tmin(handle)
 Tc = CoolProp_.AbstractState.T_critical(handle)
 
-# Function to calculate vapor pressure across a temperature range
-function calculate_vp(compound, CES)
-    N = 200
-    vp_values = zeros(N)
-
-    model = eval(Meta.parse(CES * "([" * "\"" * compound * "\"])"))
-
-    Tmax = 0.9999 * Tc
-    T = LinRange(Tmin, Tmax, N)
-
-    for i in 1:length(T)
-        vp_values[i] = saturation_pressure(model, T[i])[1]
-    end
-
-    filename_vp = "vp " * CES * " " * compound * ".npz"
-    NPZ.npzwrite(filename_vp, Dict("vp" => vp_values))
-end
-
-# Function to calculate enthalpy of vaporization across a temperature range
-function calculate_pHv(compound, CES)
-    N = 200
-
-    model = eval(Meta.parse(CES * "([" * "\"" * compound * "\"])"))
-
-    Tmax = 0.9999 * Tc
-
-    T = LinRange(Tmin, Tmax, N)
-    Hv_values = zeros(N)
-    pv_values = zeros(N)
-    v0 = nothing
-    for i in 1:length(T)
-        if i == 1
-            (pv_values[i], vl, vv) = saturation_pressure(model, T[i])
-        else
-            (pv_values[i], vl, vv) = saturation_pressure(model, T[i]; v0=v0)
-        end
-        hl = Clapeyron.VT_enthalpy(model, vl, T[i], [1.])
-        hv = Clapeyron.VT_enthalpy(model, vv, T[i], [1.])
-        v0 = (vl, vv)
-        Hv_values[i] = hv - hl
-    end
-
-    filename_Hv = "Hv " * CES * " " * compound * ".npz"
-    NPZ.npzwrite(filename_Hv, Dict("Hv" => Hv_values))
-
-    filename_pv = "pv " * CES * " " * compound * ".npz"
-    NPZ.npzwrite(filename_pv, Dict("pv" => pv_values))
-    return Hv_values, pv_values
-end
-
 # Function to calculate density matrix
 function density(compound, CES)
-    N = 6000
+    N = 200
     
     model = eval(Meta.parse(CES * "([" * "\"" * compound * "\"])"))
 
@@ -71,37 +21,72 @@ function density(compound, CES)
     pmax = CoolProp_.AbstractState.pmax(handle)
 
     T = LinRange(Tmin, Tmax, N)
-    P = LinRange(pmin, pmax, N)
+    P = exp10.(LinRange(log10(pmin), log10(pmax), N))
 
     # Preallocate the density matrix
     Phi_PR = zeros(Float64, length(T), length(P))
+    pv_values = zeros(N)
+    Hv_values = zeros(N)
+
+    v0 = nothing
+    pv = nothing
+
+    (Tc, pc, vc) = crit_pure(model)
 
     for t in T
-        for pr in P
+        println(t)
+        if t < Tc
+            if t == T[1]
+                (pv, vl, vv) = saturation_pressure(model, t)
+            else
+                (pv, vl, vv) = saturation_pressure(model, t, IsoFugacitySaturation(p0 = pv, vl = v0[2], vv = v0[1]))
+            end
 
-            density_value = 1/volume(model, pr, t)
+            hl = Clapeyron.VT_enthalpy(model, vl, t, [1.])
+            hv = Clapeyron.VT_enthalpy(model, vv, t, [1.])
+            v0 = (vl, vv)
+            Hv_values[t.==T] .= hv - hl
+            pv_values[t.==T] .= pv
+        end
+
+        for pr in P
+            if pr < pc && t < Tc
+                if pr < pv
+                    density_value = 1/volume(model, pr, t; phase = :vapor)
+                else
+                    density_value = 1/volume(model, pr, t; phase = :liquid)
+                end
+            else
+                density_value = 1/volume(model, pr, t)
+            end
+
             if isnan(density_value)
                 handle.update(CoolProp_.PT_INPUTS, pr, t)
                 density_value = 1/volume(model, pr, t, vol0=1/handle.rhomolar())
             end
             Phi_PR[T .== t, P .== pr] .= density_value
-
         end
     end
 
     filename =  CES * " " * compound * " density.npz"
     NPZ.npzwrite(filename, Dict("Phi_PR" => Phi_PR))
+
+    filename_Hv = "Hv " * CES * " " * compound * ".npz"
+    NPZ.npzwrite(filename_Hv, Dict("Hv" => Hv_values))
+
+    filename_pv = "pv " * CES * " " * compound * ".npz"
+    NPZ.npzwrite(filename_pv, Dict("pv" => pv_values))
     
-    return Phi_PR
+    return Phi_PR, Hv_values, pv_values
 end
 
 
 # Example usage:
 # To calculate and save the vapor pressure
-calculate_vp(ARGS[1], ARGS[2])
+# calculate_vp(A[1], A[2])
 
 # To calculate and save the enthalpy of vaporization
-calculate_Hv(ARGS[1], ARGS[2])
+# calculate_Hv(A[1], A[2])
 
 # To calculate and print the density matrix
-density(ARGS[1], ARGS[2])
+# density(A[1], A[2])
